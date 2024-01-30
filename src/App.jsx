88c11f35ddc2b6d1f5c55015167a64c1c109aa79
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2024-01-30 18:59:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>TikTok Clon</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
